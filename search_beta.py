78854a2cbac8d7798d10504b4964522297701679
Riccardo Resta <rolandka88@gmail.com>
2019-04-26 17:26:05 +0200

    test updated

--- search_beta.py
+++ search_beta.py
@@ -1,18 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 import re, os
 import string
 import sys
+import os.path
+import glob
+from collections import Counter
 from os import listdir
-
 path = input("Enter a name of path: ")
 keyword = input("Enter a keyword: ")
-#filename = 
-#file = open(filename, "r")
-root = os.listdir(path)
-
+num_keyword = 0
+root = glob.iglob('**/*.sh', recursive=True)
 for file in root:
-    file = os.path.join(path, file)
     text = open(file, "r")
     for line in text:
         if re.match("(.*)("+ re.escape(keyword) + r")(.*)", line):
-            print (line),
+            num_keyword = num_keyword + 1
+            print (num_keyword, "result find")
+
+print ("The numbers of occurences find are:", num_keyword)
